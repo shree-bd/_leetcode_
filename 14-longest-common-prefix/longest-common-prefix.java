f0b33ba8c122361b443aa8d772ef9e06c6f23c76
Shridhar Bhardwaj <57531390+shridharbhardwaj@users.noreply.github.com>
2024-04-02 00:05:19 -0400

    Time: 0 ms (100.00%) | Memory: 41.1 MB (92.16%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,25 +1,36 @@
+// time complexity : O(n logn n) | space complexity : O(1)
 class Solution {
     public String longestCommonPrefix(String[] strs) {
 
-        StringBuilder longPrefix = new StringBuilder();
+        // StringBuilder longPrefix = new StringBuilder();
 
-        //sort the arrays
-        Arrays.sort(strs);
+        // //sort the arrays
+        // Arrays.sort(strs);
 
-        //get the first and last strings
-        char[] first = strs[0].toCharArray();
-        char[] last = strs[strs.length-1].toCharArray();
+        // //get the first and last strings
+        // char[] first = strs[0].toCharArray();
+        // char[] last = strs[strs.length-1].toCharArray();
 
-        //start comparing the first and last word characters
+        // //start comparing the first and last word characters
 
-        for(int i=0;i<first.length;i++){
-            if(first[i]!=last[i])
-            break;
+        // for(int i=0;i<first.length;i++){
+        //     if(first[i]!=last[i])
+        //     break;
 
-            longPrefix.append(first[i]);
+        //     longPrefix.append(first[i]);
+        // }
+
+        // return longPrefix.toString();
+
+        // ***************************************************
+
+        String longPrefix = strs[0];
+        for(int i=0;i<strs.length;i++){
+            while (strs[i].indexOf(longPrefix)!=0){
+                longPrefix = longPrefix.substring(0,longPrefix.length()-1);
+            }
         }
-
-        return longPrefix.toString();
+        return longPrefix;
         
     }
 }
