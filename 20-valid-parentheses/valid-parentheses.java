2aa95a225383cf03c082190c48e04762b5e442e1
Shridhar Bhardwaj <57531390+shridharbhardwaj@users.noreply.github.com>
2024-04-03 00:12:29 -0400

    Time: 1 ms (98.64%) | Memory: 41.3 MB (62.24%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s==null || s.length() == 0){
+            return true;
+        }
+
+        // //declare and initialize the character array
+        // char[] charArray = { '(',')','{','}','[',']' };
+
+        Stack<Character> stck = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(c=='(')
+                stck.push(')');
+
+            else if(c=='{')
+                stck.push('}');
+
+            else if(c=='[')
+                stck.push(']');
+
+            else if (stck.isEmpty() || stck.pop() !=c)
+                return false;
+        }
+
+        return stck.isEmpty();
+
+        
+
+    }
+
+
+}
