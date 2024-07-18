e61d850393008671bc7ff729d69ac6813f3a35d6
Shridhar B <57531390+shridharbhardwaj@users.noreply.github.com>
2024-07-17 20:01:42 -0400

    Time: 127 ms (53.24%) | Memory: 18.2 MB (15.86%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,9 +1,28 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        count = nums.count(0)
-        nums[:] = [num for num in nums if num != 0]
-        nums.extend([0] * count)
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        
+        left = 0
+        for right in range(len(nums)):
+            if nums[right]:
+                nums[left],nums[right] = nums[right],nums[left]
+                left += 1
+        return nums
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Solution:
+#     def moveZeroes(self, nums: List[int]) -> None:
+#         count = nums.count(0)
+#         nums[:] = [num for num in nums if num != 0]
+#         nums.extend([0] * count)
