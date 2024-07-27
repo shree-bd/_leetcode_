7be7dc80d474dd13471502aab1bd9ab16ad3d3c6
Shridhar B <57531390+shridharbhardwaj@users.noreply.github.com>
2024-07-27 14:13:07 -0400

    Time: 39 ms (28.83%) | Memory: 16.6 MB (25.97%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,6 +1,5 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        word1, word2 = list(word1), list(word2)
         merged = []
 
         # Initialize indices for both words
@@ -12,17 +11,17 @@ class Solution:
             merged.append(word2[j])
             i+=1
             j+=1
+        merged.append(word1[i:])
+        merged.append(word2[j:])
+        # # Append remaining letters fro word1 if any
+        # while i < len(word1):
+        #     merged.append(word1[i])
+        #     i+=1
 
-        # Append remaining letters fro word1 if any
-        while i < len(word1):
-            merged.append(word1[i])
-            i+=1
-
-        # Append remaining letters fro word1 if any
-        while j < len(word2):
-            merged.append(word2[j])
-            j+=1
-
-        return ''.join(merged)
+        # # Append remaining letters fro word1 if any
+        # while j < len(word2):
+        #     merged.append(word2[j])
+        #     j+=1
+        return "".join(merged)
 
         
