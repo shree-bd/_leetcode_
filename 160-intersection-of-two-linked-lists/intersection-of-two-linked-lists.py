0551e378464131091fc16366edd8e1df5d9daf9d
Shridhar B <57531390+shridharbhardwaj@users.noreply.github.com>
2024-07-28 19:01:22 -0400

    Time: 75 ms (84.38%) | Memory: 27.1 MB (40.89%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
@@ -6,36 +6,41 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        if not headA or not headB:
-            return None
-        # calculate lenght of both lists
-        lengthA, lengthB = 0,0
-        currA, currB = headA, headB
+        # if not headA or not headB:
+        #     return None
+        # # calculate lenght of both lists
+        # lengthA, lengthB = 0,0
+        # currA, currB = headA, headB
 
-        while currA != None:
-            lengthA += 1
-            currA = currA.next
+        # while currA != None:
+        #     lengthA += 1
+        #     currA = currA.next
 
-        while currB != None:
-            lengthB += 1
-            currB = currB.next
+        # while currB != None:
+        #     lengthB += 1
+        #     currB = currB.next
         
-        # Align the starting point of both lists
-        currA, currB = headA, headB
-        if lengthA > lengthB:
-            for _ in range(lengthA - lengthB):
-                currA = currA.next
-        else:
-            for _ in range(lengthB - lengthA):
-                currB = currB.next
+        # # Align the starting point of both lists
+        # currA, currB = headA, headB
+        # if lengthA > lengthB:
+        #     for _ in range(lengthA - lengthB):
+        #         currA = currA.next
+        # else:
+        #     for _ in range(lengthB - lengthA):
+        #         currB = currB.next
 
-        # traverse both lists and find the intersection
-        while currA and currB:
-            if currA==currB:
-                return currA
-            currA = currA.next
-            currB = currB.next
+        # # traverse both lists and find the intersection
+        # while currA and currB:
+        #     if currA==currB:
+        #         return currA
+        #     currA = currA.next
+        #     currB = currB.next
         
-        return None
+        # return None
+        l1, l2 = headA, headB
+        while l1 != l2:
+            l1 = l1.next if l1 else headB
+            l2 = l2.next if l2 else headA
+        return l1
             
         
