b0a358fe755ef38fd82c967b94e014ee520e559d
Shridhar B <57531390+shridharbhardwaj@users.noreply.github.com>
2024-08-04 14:49:34 -0400

    Time: 535 ms (16.84%) | Memory: 0B (100.00%) - LeetSync

--- 608-tree-node/tree-node.sql
+++ 608-tree-node/tree-node.sql
@@ -1,7 +1,7 @@
-SELECT id,
-       CASE 
-           WHEN p_id IS NULL THEN 'Root'
-           WHEN id IN (SELECT DISTINCT p_id FROM Tree WHERE p_id IS NOT NULL) THEN 'Inner'
-           ELSE 'Leaf'
-       END AS type
-FROM Tree;
+select id,
+    CASE
+        WHEN p_id is null THEN 'Root'
+        WHEN id in (select p_id from Tree) THEN 'Inner'
+        ELSE 'Leaf'
+    END as type
+from Tree
