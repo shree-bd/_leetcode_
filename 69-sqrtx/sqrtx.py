9d1a8415c29daacb28e2c408038b314d25c6138d
Shridhar B <57531390+shridharbhardwaj@users.noreply.github.com>
2024-08-06 19:31:00 -0400

    Time: 27 ms (98.11%) | Memory: 16.5 MB (61.10%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -5,16 +5,25 @@ class Solution:
         
         if x == 0 or x == 1:
             return x
-        
-        # Binary search
-        left, right = 0, x
-        while left <= right:
-            mid = (left + right) // 2
-            if mid * mid == x:
-                return mid
-            elif mid * mid < x:
-                left = mid + 1
+
+        # Binary Search
+        first, last = 1, x
+        res = 0
+
+        while first <= last:
+            mid = first + ((last-first)//2)
+
+            if (mid**2) > x:
+                last = mid-1
+            elif (mid**2) < x:
+                first = mid+1
+                res = mid
             else:
-                right = mid - 1
+                return mid
+        return res
+
+
+
+
+        return last
         
-        return right
