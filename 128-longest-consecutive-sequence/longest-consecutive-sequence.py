8ce1623d29e4344cc5a5b64cb5b0963a60e7612e
Shridhar B <57531390+shridharbhardwaj@users.noreply.github.com>
2024-08-15 22:15:09 -0400

    Time: 358 ms (58.08%) | Memory: 31.7 MB (88.23%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,11 +1,10 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
+        # if not nums:
+        #     return 0
 
         nums_set = set(nums)
         longest = 0
-
 
         for num in nums_set:
             if num-1 not in nums_set: #start of a new sequence
@@ -22,4 +21,34 @@ class Solution:
         return longest
 
 
+
+# By using map:
+
+# class Solution:
+#     def longestConsecutive(self, nums: List[int]) -> int:
+        
+#         nums_map = {}
+#         max_len = 0
+
+#         for num in nums:
+
+#             if num not in nums_map:
+#                 nums_map[num] = [num, num]
+#                 left = num
+#                 right = num
+            
+#                 # look to extend the sequence [a,b] left or right
+#                 if num - 1 in nums_map:
+#                     left = nums_map[num-1][0]
+                
+#                 if num + 1 in nums_map:
+#                     right = nums_map[num+1][1]
+                
+#                 nums_map[left] = [left, right]
+#                 nums_map[right] = [left, right]
+
+#                 max_len = max(max_len, right - left + 1)
+#         print(nums_map)
+#         return max_len
+        
         
