7c77b67cfd68907b2d4f0e0f97af92cf923228d9
Shridhar B <57531390+shridharbhardwaj@users.noreply.github.com>
2024-08-21 00:52:09 -0400

    Time: 46 ms (16.23%) | Memory: 17.5 MB (9.74%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -1,30 +1,43 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+# # Definition for a binary tree node.
+# # class TreeNode:
+# #     def __init__(self, val=0, left=None, right=None):
+# #         self.val = val
+# #         self.left = left
+# #         self.right = right
+# class Solution:
+#     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+#         if not root:
+#             return []
+
+#         result = []
+
+#         q= collections.deque()
+#         q.append(root)
+
+#         while q:
+#             qLen = len(q)
+#             level = []
+#             for i in range(qLen):
+#                 node = q.popleft()
+#                 if node:
+#                     level.append(node.val)
+#                     q.append(node.left)
+#                     q.append(node.right)
+#             if level:
+#                 result.append(level)
+
+#         return result
+
+        
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+# By using DFS
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-
-        result = []
-
-        q= collections.deque()
-        q.append(root)
-
-        while q:
-            qLen = len(q)
-            level = []
-            for i in range(qLen):
-                node = q.popleft()
-                if node:
-                    level.append(node.val)
-                    q.append(node.left)
-                    q.append(node.right)
-            if level:
-                result.append(level)
-
-        return result
-
-        
-        
+        self.d=defaultdict(list)
+        def rec(x,depth):
+            self.d[depth].append(x.val)
+            if x.left: rec(x.left,depth+1)
+            if x.right: rec(x.right,depth+1)
+        if root: rec(root,0)
+        return [self.d[i] for i in sorted(self.d.keys())]
