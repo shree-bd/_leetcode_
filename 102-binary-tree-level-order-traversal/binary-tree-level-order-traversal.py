591d038f76dc0144e79ef79fea7d719fc3e1d896
Shridhar B <57531390+shridharbhardwaj@users.noreply.github.com>
2024-08-21 00:52:39 -0400

    Time: 39 ms (63.04%) | Memory: 17.3 MB (30.32%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -4,40 +4,40 @@
 # #         self.val = val
 # #         self.left = left
 # #         self.right = right
-# class Solution:
-#     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-#         if not root:
-#             return []
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
 
-#         result = []
+        result = []
 
-#         q= collections.deque()
-#         q.append(root)
+        q= collections.deque()
+        q.append(root)
 
-#         while q:
-#             qLen = len(q)
-#             level = []
-#             for i in range(qLen):
-#                 node = q.popleft()
-#                 if node:
-#                     level.append(node.val)
-#                     q.append(node.left)
-#                     q.append(node.right)
-#             if level:
-#                 result.append(level)
+        while q:
+            qLen = len(q)
+            level = []
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                result.append(level)
 
-#         return result
+        return result
 
         
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 # By using DFS
-class Solution:
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        self.d=defaultdict(list)
-        def rec(x,depth):
-            self.d[depth].append(x.val)
-            if x.left: rec(x.left,depth+1)
-            if x.right: rec(x.right,depth+1)
-        if root: rec(root,0)
-        return [self.d[i] for i in sorted(self.d.keys())]
+# class Solution:
+#     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+#         self.d=defaultdict(list)
+#         def rec(x,depth):
+#             self.d[depth].append(x.val)
+#             if x.left: rec(x.left,depth+1)
+#             if x.right: rec(x.right,depth+1)
+#         if root: rec(root,0)
+#         return [self.d[i] for i in sorted(self.d.keys())]
