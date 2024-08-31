b1629288736eccee256756398d11f9761304783a
Shridhar B <57531390+shridharbhardwaj@users.noreply.github.com>
2024-08-31 18:40:01 -0400

    Time: 589 ms (44.35%) | Memory: 17 MB (80.20%) - LeetSync

--- 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.py
+++ 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.py
@@ -11,7 +11,14 @@ class Solution:
         for col in range(n):
             if set(matrix[row][col] for row in range(n)) != set(range(1,n+1)):
                 return False
-                
+
         return True
 
         
+# class Solution:
+#     def checkValid(self, matrix: List[List[int]]) -> bool:
+#         n = len(matrix)
+#         for row, col in zip(matrix, zip(*matrix)):
+#             if len(set(row)) != n or len(set(col)) != n:
+#                 return False
+#         return True
