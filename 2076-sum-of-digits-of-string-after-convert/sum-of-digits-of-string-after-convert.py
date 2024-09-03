36c0566550028cb5ad97689d09990387bc071218
Shridhar B <57531390+shridharbhardwaj@users.noreply.github.com>
2024-09-03 00:43:11 -0400

    Time: 34 ms (81.98%) | Memory: 16.5 MB (37.00%) - LeetSync

--- 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.py
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.py
@@ -14,3 +14,10 @@ class Solution:
         return int(res)
 
         
+
+        # alist=list(string.ascii_lowercase)
+        #         val="".join(str(alist.index(i)+1) for i in s)
+        #         for _ in range(k):
+        #             val=str(sum(int(digit) for digit in val))
+
+        #         return int(val)
