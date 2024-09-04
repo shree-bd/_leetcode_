90157638bc36cb204cd645a79d7f32985dec0278
Shridhar B <57531390+shridharbhardwaj@users.noreply.github.com>
2024-09-04 00:55:16 -0400

    Time: 38 ms (29.27%) | Memory: 16.5 MB (15.30%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -5,16 +5,17 @@ class Solution:
         if n == 2:
             return 2
 
-        # initialise the base cases for DP:
-        dp = [0] * (n+1)
-        dp[1] = 1
-        dp[2] = 2
+        # Two variables to store the last two results
+        first = 1
+        second = 2
 
+        # Iteratively compute the result
+        for i in range(3, n + 1):
+            current = first + second
+            first = second
+            second = current
 
-        for  i in range(3,n+1):
-            dp[i] = dp[i-1] + dp[i-2]
-
-        return dp[n]        
+        return second 
 
 
 
