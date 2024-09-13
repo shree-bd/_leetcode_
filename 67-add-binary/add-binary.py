288edd51de0e87961b84eede8f696dd5cb7092ad
Shridhar B <57531390+shridharbhardwaj@users.noreply.github.com>
2024-09-12 23:43:45 -0400

    Time: 31 ms (89.14%) | Memory: 16.7 MB (23.31%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,13 +1,41 @@
+
+            
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        num1 = int(a, 2)
-        num2 = int(b, 2)
+        ans = ""
+        carry = 0
 
-        # Add the integers
-        sum_num = num1 + num2
+        a,b = a[::-1],b[::-1]
+        for i in range(max(len(a), len(b))):
+            digitA = ord(a[i]) - ord("0") if i < len(a) else 0
+            digitB = ord(b[i]) - ord("0") if i < len(b) else 0
 
-        # Convert the result back to a binary string and remove '0b' prefix
-        binary_sum = bin(sum_num)[2:]
+            total = digitA + digitB + carry
+            char = str(total%2)
+            ans = char + ans
+            carry = total //2
 
-        return binary_sum
-            
+        if carry:
+            ans = "1" +ans
+        return ans
+
+
+
+
+
+
+
+# by using python function
+
+# class Solution:
+#     def addBinary(self, a: str, b: str) -> str:
+#         num1 = int(a, 2)
+#         num2 = int(b, 2)
+
+#         # Add the integers
+#         sum_num = num1 + num2
+
+#         # Convert the result back to a binary string and remove '0b' prefix
+#         binary_sum = bin(sum_num)[2:]
+
+#         return binary_sum
