ead30a075260b8239f9145b048d845a2b1c64e83
Shree B <57531390+shree-bd@users.noreply.github.com>
2024-10-06 14:11:17 -0400

    Time: 37 ms (59.29%) | Memory: 16.8 MB (85.40%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,31 +1,46 @@
+# class Solution:
+#     def generateParenthesis(self, n: int) -> List[str]:
+#         # only add open parenthese if open < n
+#         # only add a closing parenthses if closed < open
+#         # valid IIF open == closed == n
+
+#         stack = []
+#         result = []
+
+#         def backtrack(open_count = 0, closed_count = 0):
+#             if open_count == closed_count == n:
+#                 result.append("".join(stack))
+#                 return
+
+#             if open_count < n:
+#                 stack.append("(")
+#                 backtrack(open_count+1, closed_count)
+#                 stack.pop()
+
+#             if closed_count < open_count:
+#                 stack.append(")")
+#                 backtrack(open_count, closed_count+1)
+#                 stack.pop()
+
+#         backtrack(0,0)
+#         return result
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        # only add open parenthese if open < n
-        # only add a closing parenthses if closed < open
-        # valid IIF open == closed == n
+        def dfs(left, right, s):
+            if len(s) == n * 2:
+                res.append(s)
+                return 
 
-        stack = []
-        result = []
+            if left < n:
+                dfs(left + 1, right, s + '(')
 
-        def backtrack(open_count, closed_count):
-            if open_count == closed_count == n:
-                result.append("".join(stack))
-                return
+            if right < left:
+                dfs(left, right + 1, s + ')')
 
-            if open_count < n:
-                stack.append("(")
-                backtrack(open_count+1, closed_count)
-                stack.pop()
-
-            if closed_count < open_count:
-                stack.append(")")
-                backtrack(open_count, closed_count+1)
-                stack.pop()
-
-        backtrack(0,0)
-        return result
-
-
-
-
-        
+        res = []
+        dfs(0, 0, '')
+        return res
