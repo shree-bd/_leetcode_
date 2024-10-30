617262a9384f3c64e5e917cd18649141ca38b896
Shree B <57531390+shree-bd@users.noreply.github.com>
2024-10-30 10:23:54 -0400

    Time: 69 ms (90.53%) | Memory: 73.6 MB (76.09%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.py
+++ 1023-time-based-key-value-store/time-based-key-value-store.py
@@ -1,30 +1,58 @@
+# class TimeMap:
+
+#     def __init__(self):
+#         self.store = {}
+        
+
+#     def set(self, key: str, value: str, timestamp: int) -> None:
+#         if key not in self.store:
+#             self.store[key] = []
+#         self.store[key].append([value, timestamp])
+     
+#     def get(self, key: str, timestamp: int) -> str:
+#         res = ""
+#         values = self.store.get(key, [])
+
+#         # binary search
+#         l, r = 0, len(values) - 1
+#         while l <= r:
+#             m = (l+r)//2
+#             if values[m][1] <= timestamp:
+#                 res = values[m][0]
+#                 l = m+1
+#             else:
+#                 r = m-1
+#         return res
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+from collections import defaultdict, deque
 class TimeMap:
 
     def __init__(self):
-        self.store = {}
+        self.map = defaultdict(deque)
         
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.store:
-            self.store[key] = []
-        self.store[key].append([value, timestamp])
-     
-    def get(self, key: str, timestamp: int) -> str:
-        res = ""
-        values = self.store.get(key, [])
-
-        # binary search
-        l, r = 0, len(values) - 1
-        while l <= r:
-            m = (l+r)//2
-            if values[m][1] <= timestamp:
-                res = values[m][0]
-                l = m+1
-            else:
-                r = m-1
-        return res
-
+        self.map[key].append((timestamp, value))
         
+
+    def get(self, key: str, timestamp: int) -> str:
+        if self.map[key] and timestamp >= self.map[key][-1][0]:
+            print(self.map[key][-1][1])
+            return self.map[key][-1][1]
+        val = ""
+        l, r = 0, len(self.map[key]) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if self.map[key][m][0] == timestamp:
+                return self.map[key][m][1]
+            if self.map[key][m][0] > timestamp:
+                r = m - 1
+            else:
+                val = self.map[key][m][1]
+                l = m + 1
+        return val
 
 
 # Your TimeMap object will be instantiated and called as such:
