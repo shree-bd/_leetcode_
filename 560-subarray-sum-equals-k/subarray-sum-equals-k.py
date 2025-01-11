a1654e59d306addbea028f2601ab70a38fc0ad65
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-01-11 13:54:22 -0500

    Time: 30 ms (65.85%) | Memory: 20.5 MB (17.64%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -2,16 +2,21 @@ class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         result = 0
         curr_sum = 0
+
+        # Dictionary to store prefix sums and their counts
+        # Starts with {0: 1} to handle the case where a subarray starting from the beginning equals k
         prefix_sums = {0:1}
 
+        # Iterate over each number in the array
         for n in nums:
             curr_sum += n
             diff = curr_sum - k
-
+            
+            
+            # If the difference exists in prefix_sums, add its count to the result
+            # This means there are 'prefix_sums[diff]' subarrays that end at the current index and sum to k
             result += prefix_sums.get(diff, 0)
             prefix_sums[curr_sum] = 1 + prefix_sums.get(curr_sum, 0)
 
 
         return result
-
-        
