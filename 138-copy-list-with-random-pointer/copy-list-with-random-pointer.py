b08f01f060f25e81f0bbd1828930e25470f68fab
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-01-12 17:24:57 -0500

    Time: 44 ms (27.05%) | Memory: 18.7 MB (18.29%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -1,3 +1,6 @@
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Using Opimized One Pass Approach <<<<<<<<<<<<<<<<<<<<<<<<<<<
+
 """
 # Definition for a Node.
 class Node:
@@ -7,6 +10,52 @@ class Node:
         self.random = random
 """
 
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return None
+
+        # hashmap to store mapping from org nodes to their corresponding copied nodes
+        old_to_new = {}
+
+
+        # Helper function 
+        def get_cloned(node):
+            if not node:
+                return None
+            
+            if node not in old_to_new:
+                # If not yet created, create and store the new node
+                old_to_new[node] = Node(node.val)
+
+            return old_to_new[node]
+
+        # Start iterating through the original list
+        curr = head
+        while curr:
+            copied_node = get_cloned(curr)
+
+            copied_node.next = get_cloned(curr.next)
+            copied_node.random = get_cloned(curr.random)
+
+            # Move to the next node
+            curr = curr.next
+
+        # return the head of the copied list
+        return old_to_new[head]
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Using Two Passes Approach <<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+"""
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if not head:
@@ -31,3 +80,7 @@ class Solution:
 
         
         return old_to_new[head]
+"""
+
+# Time complexity:  O(n)
+# Space Complexity: O(n)
