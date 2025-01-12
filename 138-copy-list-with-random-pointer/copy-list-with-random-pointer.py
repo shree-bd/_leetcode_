fc9f0e612341135d149f75a3774445574540959c
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-01-12 18:06:38 -0500

    Time: 31 ms (94.78%) | Memory: 18.7 MB (18.29%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -1,3 +1,54 @@
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Using Interweaving Approach <<<<<<<<<<<<<<<<<<<<<<<<<<<
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return None
+
+        # Step 1: Interweave the copied nodes with the original nodes
+        current = head
+        while current:
+            new_node = Node(current.val)
+            new_node.next = current.next
+            current.next = new_node
+            current = new_node.next
+
+        # Step 2: Assign random pointers for the copied nodes
+        current = head
+        while current:
+            if current.random:
+                current.next.random = current.random.next
+            current = current.next.next
+
+        # Step 3: Separate the original and copied lists
+        original = head
+        copy = head.next
+        copy_head = copy
+
+        while original:
+            original.next = original.next.next
+            if copy.next:
+                copy.next = copy.next.next
+            original = original.next
+            copy = copy.next
+
+        return copy_head
+            
+
+
+
+
+
+# Time complexity:  O(n)
+# Space Complexity: O(1)
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Using Opimized One Pass Approach <<<<<<<<<<<<<<<<<<<<<<<<<<<
 
@@ -9,7 +60,7 @@ class Node:
         self.next = next
         self.random = random
 """
-
+"""
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if not head:
@@ -43,7 +94,10 @@ class Solution:
 
         # return the head of the copied list
         return old_to_new[head]
+"""
 
+# Time complexity:  O(n)
+# Space Complexity: O(n)
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Using Two Passes Approach <<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 
