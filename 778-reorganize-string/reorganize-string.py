fd519dc73133793a0cb4435074b8077f29960608
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-01-30 00:14:37 -0500

    Time: 2 ms (58.94%) | Memory: 17.9 MB (20.70%) - LeetSync

--- 778-reorganize-string/reorganize-string.py
+++ 778-reorganize-string/reorganize-string.py
@@ -33,3 +33,39 @@ class Solution:
 
 # Time Complexity: O(n) (where n is the length of the input string)
 # Space Complexity: O(n) (since the primary space is used by the result string)
+
+
+# Other good solution
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        #we want to pair the ones with the highest frequency first
+        #each time we will pop the first two items from the heap
+        #we only add it back to the heap if freq + 1 is > 0, meaning we still have letters to pair
+        #our heap will be while len(heap) > 1, while our heap has more than one letter
+        #now that our heap has only one letter left, there must be one count of that letter, if there is 
+        #more than one, we cannot place it side by side
+        counts = Counter(s)
+        maxHeap = [(-freq, letter) for letter, freq in counts.items()]
+        heapify(maxHeap)
+
+        res = []
+        while len(maxHeap) > 1:
+            freq1, char1 = heappop(maxHeap)
+            freq2, char2 = heappop(maxHeap)
+            
+            res.extend([char1, char2])
+
+            if freq1 + 1 < 0:
+                heappush(maxHeap, (freq1 + 1, char1))
+            if freq2 + 1 < 0:
+                heappush(maxHeap, (freq2 + 1, char2))
+        
+        if maxHeap:
+            freq, char = heappop(maxHeap)
+            if freq + 1 < 0:
+                return ""
+            res.append(char)
+        
+        return "".join(res)
+            
+            
