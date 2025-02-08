9076f57c06fcf48b7f4074c84f014bf2fb881748
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-02-07 23:45:47 -0500

    Time: 301 ms (33.33%) | Memory: 95.3 MB (33.82%) - LeetSync

--- 2434-design-a-number-container-system/design-a-number-container-system.py
+++ 2434-design-a-number-container-system/design-a-number-container-system.py
@@ -1,30 +1,30 @@
 class NumberContainers:
-
     def __init__(self):
-        # Initializing the defaultdict with SortedSet and the regular dictionary
-        # Map from number to set of indices
-        self.number_to_indices = collections.defaultdict(SortedSet)
-        # Map from index to number
-        self.index_to_number = {}
+        # Dictionary to map index to the number
+        self.indexToNum = {}
+        
+        # Dictionary to map number to a SortedList of indices
+        self.numToIndices = {}
 
     def change(self, index: int, number: int) -> None:
-        # If index already has a number, remove it from the old number's index set
-        if index in self.index_to_number:
-            previous_number = self.index_to_number[index]
-            self.number_to_indices[previous_number].remove(index)
-            if not self.number_to_indices[previous_number]:
-                del self.number_to_indices[previous_number]
-
-        # Update the number and add the index to the new number's set
-        self.index_to_number[index] = number
-        self.number_to_indices[number].add(index)
+        # If index already exists, remove the old number from the list of indices
+        if index in self.indexToNum:
+            old_number = self.indexToNum[index]
+            self.numToIndices[old_number].remove(index)
+        
+        # Update the index-to-number mapping
+        self.indexToNum[index] = number
+        
+        # Add the index to the SortedList of the new number
+        if number not in self.numToIndices:
+            self.numToIndices[number] = SortedList()
+        self.numToIndices[number].add(index)
 
     def find(self, number: int) -> int:
-        # Return the smallest index for the given number, or -1 if not found
-        if number in self.number_to_indices and self.number_to_indices[number]:
-            return self.number_to_indices[number][0]
-        return -1
-
+        # Return the smallest index for the given number (if it exists)
+        if number in self.numToIndices and self.numToIndices[number]:
+            return self.numToIndices[number][0]  # Return the smallest valid index
+        return -1  # If no valid index found
         
 # Time Complexity: O(log N)
 # Space Complexity: O(N)
