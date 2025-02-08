099a186f33cc587d38fc93459f41b2a0ce61bbdf
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-02-08 13:44:02 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (62.48%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -1,12 +1,14 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
         stack = []
-
+        # this ignores multiple s[aces automatically]
         words = s.split()
-
+        #push words onto stack
         for word in words:
             stack.append(word)
-
-
+        #pop words in reversed order
         return " ".join(stack[::-1])
-        
+
+
+# Time Complexity: O(N)
+# Space Complexity: O(N)
