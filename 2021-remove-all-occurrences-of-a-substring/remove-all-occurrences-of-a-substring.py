73dc68f5dbd12926a598263e8a12592179483d26
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-02-11 17:01:10 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (52.02%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.py
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.py
@@ -1,8 +1,16 @@
 class Solution:
     def removeOccurrences(self, s: str, part: str) -> str:
-        stack, sub_len = [], len(part)
-        for char in s:
-            stack.append(char)
-            if "".join(stack[-sub_len:]) == part:
-                del stack[-sub_len:]
-        return "".join(stack)
+        # stack, sub_len = [], len(part)
+        # for char in s:
+        #     stack.append(char)
+        #     if "".join(stack[-sub_len:]) == part:
+        #         del stack[-sub_len:]
+        # return "".join(stack)
+
+
+        while 1:
+            if part in s:
+                s=s.replace(part, "", 1)
+            else:
+                break
+        return s
