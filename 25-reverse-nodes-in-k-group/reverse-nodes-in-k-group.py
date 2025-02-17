fd48ec2fa1cecdcd0035ecd1e6dfa779a5414bba
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-02-16 21:25:17 -0500

    Time: 3 ms (32.52%) | Memory: 18.5 MB (60.78%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
@@ -5,18 +5,18 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        dummy = ListNode(0, head)
-        groupPrev = dummy
+        dummy = ListNode(0, head) # dummy node to handle edge cases
+        groupPrev = dummy   # pointer to the node before the current group
 
         while True:
+            # find the kth node in the current group
             kth = self.getKth(groupPrev, k)
-            if not kth:
+            if not kth: # if there are fewer than k nods left, exit loop
                 break
-            groupNext = kth.next
+            groupNext = kth.next   # pointer to next group's first node
 
             #reverse group
             prev, curr = kth.next, groupPrev.next
-
             while curr != groupNext:
                 tmp = curr.next
                 curr.next = prev
@@ -26,9 +26,8 @@ class Solution:
             tmp = groupPrev.next
             groupPrev.next = kth
             groupPrev = tmp
-            
-        return dummy.next
 
+        return dummy.next       # Return new head
 
     def getKth(self, curr, k):
         while curr and k > 0:
@@ -37,6 +36,8 @@ class Solution:
         return curr
 
 
+# Time Complexity: O(N)
+# Space Complexity: O(1)
 
 
         
