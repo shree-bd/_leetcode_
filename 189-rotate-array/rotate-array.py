9dec9cac6b2e21faeb78ba24cee4d1b4df391578
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-03-02 17:01:09 -0500

    Time: 3 ms (71.54%) | Memory: 25.6 MB (46.45%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -6,6 +6,24 @@ class Solution:
         n = len(nums)
         k = k%n
 
-        nums.reverse()
-        nums[:k] = reversed(nums[:k]) 
-        nums[k:] = reversed(nums[k:])
+        # nums.reverse()
+        # nums[:k] = reversed(nums[:k]) 
+        # nums[k:] = reversed(nums[k:])
+
+
+
+# >>>>>>>>>>>>> 2nd method <<<<<<<<<<<<<<<<
+
+        def rev(l, r):
+            while l < r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+                r -= 1
+
+        rev(0, n-1)
+        rev(0, k-1)
+        rev(k, n-1)
+
+        return nums
+
+
