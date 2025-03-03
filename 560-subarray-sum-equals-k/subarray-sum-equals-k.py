f5c4aa2429d2479171f1a6810258326d30b5e72e
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-03-02 21:20:33 -0500

    Time: 41 ms (25.67%) | Memory: 21.9 MB (15.68%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -3,16 +3,14 @@ class Solution:
         count = 0
         curr_sum = 0
         prefix_sums = defaultdict(int)  
-        prefix_sums[0] = 1  # Initialize for cases where sum itself is `k`
+        prefix_sums[0] = 1  
 
         for num in nums:
-            curr_sum += num  # Compute prefix sum
-            if curr_sum - k in prefix_sums:  # Check if there's a subarray sum `k`
-                count += prefix_sums[curr_sum - k]
-            prefix_sums[curr_sum] += 1  # Store occurrences of prefix sum
+            curr_sum += num  
+            count += prefix_sums[curr_sum - k]  # No need for explicit check
+            prefix_sums[curr_sum] += 1  
 
         return count
-
 
 
 
