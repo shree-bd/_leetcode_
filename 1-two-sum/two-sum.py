3fb334aa91458d6dcf44beaeb40f569922af631a
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-03-06 21:44:28 -0500

    Time: 3 ms (51.39%) | Memory: 18.8 MB (62.04%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -3,13 +3,15 @@ class Solution:
         if not nums:
             return None
 
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
+        hashMap = {}
+
+        for i,num in enumerate(nums):
+            diff = target - num
+            if diff in hashMap:
+                return [hashMap[diff], i]
+            hashMap[num] = i
 
         return []
-                    
-                    
+
         
         
