e4de2cf0853cfaf1bb15fafd18ba562025361f79
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-03-08 00:23:39 -0500

    Time: 7 ms (46.64%) | Memory: 19.1 MB (9.66%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,9 +3,18 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # temp = [num for num in nums if num!= 0]
+        # temp += [0] * (len(nums) - len(temp))        
+        # nums[:] = temp
 
-        temp = [num for num in nums if num!= 0]
-        temp += [0] * (len(nums) - len(temp))
-        
-        nums[:] = temp
+        # leave zero as where they are, just shift non zero to the left by traversing right pointer
+
+        l = 0
+        for r in range(len(nums)):
+            if nums[r] != 0:
+                nums[r], nums[l] = nums[l], nums[r]
+                l += 1
+        return nums
+
+
         
