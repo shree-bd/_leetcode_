829bd2c548875053a427b06e2fbcd46d64d330ff
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-03-08 01:24:18 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (32.40%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,13 +1,11 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # stack : LIFO
         stack = []
-
-        close_to_open = {')':'(', '}':'{', ']':'['}
+        hashMap = {")" : "(", "]":"[", "}":"{"}
 
         for char in s:
-            if char in close_to_open:
-                if stack and stack[-1] == close_to_open[char]:
+            if char in hashMap:
+                if stack and stack[-1] == hashMap[char]:
                     stack.pop()
                 else:
                     return False
@@ -17,7 +15,8 @@ class Solution:
         return True if not stack else False
 
 
-
-
-
+# Time Complexity: O(N)
+# Space Complexity: O(N)
+        
+ 
         
