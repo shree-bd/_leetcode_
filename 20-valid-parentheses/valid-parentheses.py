e0cb53b54ea60c4674816f8a3a77edbae8af89de
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-03-08 12:53:43 -0500

    Time: 3 ms (21.66%) | Memory: 17.8 MB (51.09%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,7 +1,7 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        hashMap = {")" : "(", "]": "[", "}": "{"}
         stack = []
-        hashMap = {")" : "(", "]":"[", "}":"{"}
 
         for char in s:
             if char in hashMap:
@@ -13,6 +13,37 @@ class Solution:
                 stack.append(char)
 
         return True if not stack else False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # stack = []
+        # hashMap = {")" : "(", "]":"[", "}":"{"}
+
+        # for char in s:
+        #     if char in hashMap:
+        #         if stack and stack[-1] == hashMap[char]:
+        #             stack.pop()
+        #         else:
+        #             return False
+        #     else:
+        #         stack.append(char)
+
+        # return True if not stack else False
 
 
 # Time Complexity: O(N)
