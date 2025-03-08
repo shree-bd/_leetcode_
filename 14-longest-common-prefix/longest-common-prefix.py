bbebb3953ad241f99a7a2f653c06c6d7e6838842
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-03-08 02:10:31 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (30.64%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -3,13 +3,32 @@ class Solution:
         if not strs:
             return ""
 
-        strs.sort()
-        first, last = strs[0], strs[-1]
-        i = 0
+        prefix = strs[0]  # Start with the first word as the prefix
 
-        while i < len(first) and i < len(last) and first[i] == last[i]:
-            i +=1
+        for word in strs[1:]:  # Compare with all other words
+            while not word.startswith(prefix):  # Keep reducing prefix until match
+                prefix = prefix[:-1]  # Remove last character
+                if not prefix:
+                    return ""  # If prefix becomes empty, return ""
 
-        return first[:i]
+        return prefix
+
+
+
+
+
+
+
+            # OR
+
+        # strs.sort()
+        # first, last = strs[0], strs[-1]
+        # i = 0
+
+        # while i < len(first) and i < len(last) and first[i] == last[i]:
+        #     i +=1
+        # return first[:i]
         
         
+# Time Complexity: O(N log N)
+# Space Complexity: O(1)
