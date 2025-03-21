135c41df6950ec0bb61ad0a4c5d068b2ac6cffb3
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-03-21 00:20:14 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (40.43%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,17 +1,27 @@
-class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        result = []
+# class Solution:
+#     def subsets(self, nums: List[int]) -> List[List[int]]:
+#         result = []
 
-        def backtrack(start, subset):
-            result.append(subset[:])
+#         def backtrack(start, subset):
+#             result.append(subset[:])
 
-            for i in range(start, len(nums)):
-                subset.append(nums[i])
-                backtrack(i+1, subset)
-                subset.pop()
+#             for i in range(start, len(nums)):
+#                 subset.append(nums[i])
+#                 backtrack(i+1, subset)
+#                 subset.pop()
 
-        backtrack(0,[])
-        return result
+#         backtrack(0,[])
+#         return result
 
 # Time Complexity: O(2^N)
 # Space Complexity: O(N)   
+
+# Iterative approach
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = [[]]
+        
+        for num in nums:
+            result += [subset + [num] for subset in result]
+        
+        return result
