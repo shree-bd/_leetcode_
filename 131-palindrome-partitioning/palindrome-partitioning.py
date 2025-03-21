8d23af87c92e9a20379edfa5433cc953f4166fb7
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-03-21 18:33:22 -0400

    Time: 49 ms (62.08%) | Memory: 34.5 MB (44.65%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -0,0 +1,22 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        result = []
+
+        def isPalindrome(substring):
+            return substring == substring[::-1]
+            
+        def backtrack(start, path):
+            if start == len(s):
+                result.append(path[:])
+                return
+
+            for end in range(start+1, len(s)+1):
+                substring = s[start:end]
+                if isPalindrome(substring):
+                    path.append(substring)
+                    backtrack(end, path)
+                    path.pop()
+
+        backtrack(0, [])
+        return result
+        
