f5feda35d1d91a3836c8abe06f7357529001afb0
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-03-22 11:59:02 -0400

    Time: 4 ms (39.64%) | Memory: 18.7 MB (19.01%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -3,6 +3,37 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        rows, cols = len(matrix), len(matrix[0])
+        rowZero = False
+
+        for r in range(rows):
+            for c in range(cols):
+                if matrix[r][c] == 0:
+                    matrix[0][c] = 0
+                    if r > 0:
+                        matrix[r][0] = 0
+                    else:
+                        rowZero = True
+        
+        for r in range(1, rows):
+            for c in range(1, cols):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
+                    matrix[r][c] = 0
+
+        if matrix[0][0] == 0:
+            for r in range(rows):
+                matrix[r][0] = 0
+
+        if rowZero:
+            for c in range(cols):
+                matrix[0][c] = 0
+
+
+
+
+# BRUTE FORCE SOLUTION
+
+        """
         ROWS, COLS = len(matrix), len(matrix[0])
         copy = [[matrix[i][j] for j in range(COLS)] for i in range(ROWS)]
 
@@ -17,3 +48,8 @@ class Solution:
         for i in range(ROWS):
             for j in range(COLS):
                 matrix[i][j] = copy[i][j]
+        
+# Time Complexity: O(M*N)
+# Space Complexity: O(M*N)
+
+"""
