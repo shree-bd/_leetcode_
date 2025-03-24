8a43422c61aee31f0ff93c10ae9fc421a288c208
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-03-24 17:52:53 -0400

    Time: 43 ms (53.89%) | Memory: 25.2 MB (91.66%) - LeetSync

--- 398-random-pick-index/random-pick-index.py
+++ 398-random-pick-index/random-pick-index.py
@@ -1,18 +1,50 @@
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>reservoir sampling<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 class Solution:
 
     def __init__(self, nums: List[int]):
-        self.hash_map = {}
-        for i, num in enumerate(nums):
-            if num not in self.hash_map:
-                self.hash_map[num] = []
-            self.hash_map[num].append(i)
-        
+        self.nums = nums
+        self.indices = defaultdict(list)
 
     def pick(self, target: int) -> int:
-        return random.choice(self.hash_map[target])
+        if target in self.indices:
+            return self.indices[target][
+                randint(0, len(self.indices[target])-1)
+            ]
 
+        count = 0
+        result = None
+
+        # Iterate over the nums array and apply reservoir sampling
+        for i, num in enumerate(self.nums):
+            if num == target:
+                # Reservoir sampling: pick with probability 1/count
+                if result is None or random.randint(0, count) == 0:
+                    result = i
+                count += 1
+
+            self.indices[num].append(i)
+        return result
+
+
+# Time Complexity: O(N) per pick call
+# Space Complexity: O(1)
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>brute force(hash_map) approach<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# class Solution:
+
+#     def __init__(self, nums: List[int]):
+#         self.hash_map = {}
+#         for i, num in enumerate(nums):
+#             if num not in self.hash_map:
+#                 self.hash_map[num] = []
+#             self.hash_map[num].append(i)
         
 
+#     def pick(self, target: int) -> int:
+#         return random.choice(self.hash_map[target])
+
+# Time Complexity: O(N)
+# Space Complexity: O(N)
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(nums)
