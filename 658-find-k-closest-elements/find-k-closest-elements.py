42c41f354265a0f36cb1c9272ead94719d79aece
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-03-24 14:41:29 -0400

    Time: 74 ms (15.02%) | Memory: 19.7 MB (18.44%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.py
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -1,5 +1,31 @@
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>min heap approach<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        arr.sort(key = lambda num: abs(num-x))
-        return sorted(arr[:k])
+        min_heap = []
+
+        for num in arr:
+            heapq.heappush(min_heap, (abs(num-x), num)) # stores (diff,element)
+
+        res = []
+        while k > 0:
+            res.append(heapq.heappop(min_heap)[1])
+            k -= 1        
+
+        return sorted(res)
+
+# Time Complexity: O(N log N)
+# Space Complexity: O(K)
+        
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>sorting approach<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+# class Solution:
+#     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+#         arr.sort(key = lambda num: abs(num-x))
+#         return sorted(arr[:k])
+
+# Time Complexity: O(N log N)
+# Space Complexity: O(K)
         
