223b99fc2e512aff9c35b933ed4c9f6c9a07a9e3
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-03-24 14:45:17 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (33.25%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.py
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -1,23 +1,37 @@
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>binary search approach<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        l, r = 0, len(arr)-k
 
+        while l < r:
+            mid = (l+r)//2
+            if x - arr[mid] > arr[mid+k] - x:
+                l = mid + 1
+            else:
+                r = mid
+        return arr[l:l+k]
+            
+# Time Complexity: O(log(N-k) + k)
+# Space Complexity: O(1)
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>min heap approach<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-class Solution:
-    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        min_heap = []
+# class Solution:
+#     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+#         min_heap = []
 
-        for num in arr:
-            heapq.heappush(min_heap, (abs(num-x), num)) # stores (diff,element)
+#         for num in arr:
+#             heapq.heappush(min_heap, (abs(num-x), num)) # stores (diff,element)
 
-        res = []
-        while k > 0:
-            res.append(heapq.heappop(min_heap)[1])
-            k -= 1        
+#         res = []
+#         while k > 0:
+#             res.append(heapq.heappop(min_heap)[1])
+#             k -= 1        
 
-        return sorted(res)
+#         return sorted(res)
 
-# Time Complexity: O(N log N)
-# Space Complexity: O(K)
+# Time Complexity: O(K log N)
+# Space Complexity: O(N)
         
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>sorting approach<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
