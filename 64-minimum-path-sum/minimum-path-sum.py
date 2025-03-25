0ce5cc6e78b79f067c6abcf9cc5e3c620ab4de36
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-03-24 20:21:30 -0400

    Time: 15 ms (58.44%) | Memory: 20.1 MB (61.40%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,3 +1,26 @@
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>in O(1) space complexity<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+
+        for r in range(rows-1, -1, -1):
+            for c in range(cols-1, -1, -1):
+                if r == rows-1 and c == cols-1:
+                    continue # skip bottom right corner
+                elif r == rows-1:
+                    grid[r][c] += grid[r][c+1] # last rows
+                elif c == cols-1:
+                    grid[r][c] += grid[r+1][c] # last column
+                else:
+                    grid[r][c] += min(grid[r+1][c], grid[r][c+1]) #choose min path
+        return grid[0][0]
+
+# Time Complexity: O(M*N)
+# Space Complexity: O(1)
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>reservoir sampling<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+"""
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
@@ -10,4 +33,5 @@ class Solution:
         return res[0][0]
 
 # Time Complexity: O(M*N)
-# Space Complexity: O(1)
+# Space Complexity: O(M*n)
+"""
