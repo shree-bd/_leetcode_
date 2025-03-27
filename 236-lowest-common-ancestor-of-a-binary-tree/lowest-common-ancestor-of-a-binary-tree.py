f7dc4a9923ca0d990236ded98fc6f18bd93dc162
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-03-26 20:39:32 -0400

    Time: 50 ms (79.79%) | Memory: 22.1 MB (71.19%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -18,6 +18,33 @@ class Solution:
         else:
             return l or r
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # if not root or root == p or root == q:
+        #     return root
+
+        # l = self.lowestCommonAncestor(root.left, p, q)
+        # r = self.lowestCommonAncestor(root.right, p, q)
+
+        # if l and r:
+        #     return root  # If p and q are found in different subtrees, root is LCA
+        # else:
+        #     return l or r   # If both are in the same subtree, return the one that is non-null
+
+
 # Time Complexity: O(N)
 # Space Complexity: O(H)
 
