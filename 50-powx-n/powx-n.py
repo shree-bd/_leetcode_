e16d42c9e202754fd077b0c459bfce07feca4290
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-03-29 20:47:35 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (94.43%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,17 +1,46 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        # x ^ -n = 1/x^
-        if x == 0: return 0
+        # x ^ -n = 1/x^n
+        if x == 0: return 0       
 
-        def helper(x,n):            
+        def dnc(x,n):
             if n == 0: return 1
 
-            # Recursive case: Compute the result for the smaller problem (x^(n//2))
-            res = helper(x * x, n // 2)              # Use x^2 and halve n
-            return x * res if n % 2 else res
+            res = dnc(x*x, n//2)
+            return x * res if n%2 else res
 
-        res = helper(x, abs(n))
-        return res if n>= 0 else 1/res
+        res = dnc(x, abs(n))
+        return res if n>0 else 1/res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # if x == 0: return 0
+
+        # def helper(x,n):            
+        #     if n == 0: return 1
+
+        #     # Recursive case: Compute the result for the smaller problem (x^(n//2))
+        #     res = helper(x * x, n // 2)              # Use x^2 and halve n
+        #     return x * res if n % 2 else res
+
+        # res = helper(x, abs(n))
+        # return res if n>= 0 else 1/res
 
 
 
