3cc3d2f145b1584e6e7eaa0e18024e5db9c7e6cf
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-04-03 19:27:58 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (76.90%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -1,22 +1,36 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
         stack = []
-        curr = ""
 
+        parts = path.split("/")
 
-        for c in path + "/":
-            if c == "/":                        # Slash marks the end of a directory name
-                if curr =="..":                 # Handle 'go to parent directory'
-                    if stack: stack.pop()
-                elif curr != "" and curr != ".":
-                    stack.append(curr)
-                # Reset the current directory name for the next one
-                curr = ""
+        for part in parts:
+            if part == "" or part == ".":
+                continue
+            elif part == "..":
+                if stack:
+                    stack.pop()
             else:
-                # Build the current directory name character by character
-                curr += c
+                stack.append(part)
 
         return "/" + "/".join(stack)
 
+        #  /home/a/b
+
+
              
-        
+# Time Complexity: O(N)
+# Space Complexity: O(N)
+
+"""
+Input: "/a/./b/../../c/"
+Steps:
+- 'a' → push to stack
+- '.' → ignore
+- 'b' → push to stack
+- '..' → pop 'b'
+- '..' → pop 'a'
+- 'c' → push to stack
+
+Output: "/c"
+"""
