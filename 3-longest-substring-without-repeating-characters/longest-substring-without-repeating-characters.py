a22b8823b960f2793258d1cb6131ed9a6ebd95c3
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-04-03 00:48:17 -0400

    Time: 20 ms (41.02%) | Memory: 17.9 MB (34.32%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -3,14 +3,19 @@ class Solution:
         char_set = set()
         left = 0
         res = 0
-
+        
         for right in range(len(s)):
             while s[right] in char_set:
                 char_set.remove(s[left])
                 left += 1
             char_set.add(s[right])
             res = max(res, right - left + 1)
+
         return res
+
+
+            
+
 
 # Time Complexity: O(N)
 # Space Complexity: O(N)
