bf43d8556ea2a77306ec12dc0e88a42abfb365f9
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-04-10 22:51:03 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (35.75%) - LeetSync

--- 851-goat-latin/goat-latin.py
+++ 851-goat-latin/goat-latin.py
@@ -6,17 +6,13 @@ class Solution:
 
         for i, word in enumerate(words, 1):
             if word[0] not in vowels:
-                word = word[1:] + word[0] + "ma"
+                goat = word[1:] + word[0] + "ma"
             else:
-                word += "ma"
-            
-            word += "a" * i
-            res.append(word)
+                goat = word + "ma"
+
+            res.append(goat + "a" * i)
 
         return " ".join(res)
-
-
-
 
             
 # Time Complexity: O(N)
