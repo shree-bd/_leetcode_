5b20e5e6497820ebf1499ab0c2541ecde17f1a9e
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-04-10 22:18:52 -0400

    Time: 4 ms (2.05%) | Memory: 17.9 MB (14.57%) - LeetSync

---
+++ 851-goat-latin/goat-latin.py
@@ -0,0 +1,20 @@
+class Solution:
+    def toGoatLatin(self, sentence: str) -> str:
+        words = sentence.split()
+        vowels = ["a", "e", "i", "o", "u", "A", "E","I","O","U"]
+        res = []
+
+        for i in range(len(words)):
+            word = words[i]
+            if word[0] in vowels:
+                word += "ma"
+            else:
+                word = word[1:] + word[0] + "ma"
+            word += "a" * (i+1)
+            res.append(word)
+
+        return " ".join(res)
+
+            
+# Time Complexity: O()
+# Space Complexity: O()
