413f3a37085900e2ebaf9b0a8b79618c37ef83d9
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-04-12 16:26:02 -0400

    Time: 1 ms (75.07%) | Memory: 18.1 MB (94.24%) - LeetSync

--- 1184-car-pooling/car-pooling.py
+++ 1184-car-pooling/car-pooling.py
@@ -2,6 +2,24 @@ class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
         change = [0] * 1001
 
+        for passengers, start, end in trips:
+            change[start] += passengers
+            change[end] -= passengers
+
+        curr = 0
+        for c in change:
+            curr += c
+            if curr > capacity: return False
+        return True
+
+# Time Complexity: O(N) | Space Complexity: O(R) R -> diff array of size 1001
+
+"""
+# BRUTE FORCE
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        change = [0] * 1001
+
         for passenger, start, end in trips:
             for i in range(start, end):
                 change[i] += passenger
@@ -10,3 +28,6 @@ class Solution:
 
         return True
         
+# Time Complexity: O(T*R) | Space Complexity: O(1)
+
+"""
