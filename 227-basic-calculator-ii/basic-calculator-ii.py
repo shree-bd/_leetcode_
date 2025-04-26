1af9c14fc53751b1bb8f93474cf2e647b530bbe6
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-04-26 18:41:32 -0400

    Time: 59 ms (77.90%) | Memory: 18.8 MB (98.20%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,5 +1,37 @@
 class Solution:
     def calculate(self, s: str) -> int:
+        s = s.replace(" ", "")
+        curr_sign = "+"
+        num = 0
+        last_num = 0
+        result = 0
+
+        for i, c  in enumerate(s):
+            if c.isdigit():
+                num = num * 10 + int(c)
+
+            if c in "+-/*" or i == len(s) - 1:
+                if curr_sign == "+":
+                    result += last_num
+                    last_num = num
+                elif curr_sign == "-":
+                    result += last_num
+                    last_num = -num
+                elif curr_sign == "*":
+                    last_num = last_num * num
+                elif curr_sign == "/":
+                    if last_num < 0:
+                        last_num = -(-last_num//num)
+                    else:
+                        last_num = last_num // num
+                curr_sign = c
+                num = 0
+
+        result += last_num
+        return result
+
+
+        """
         s = s.replace(' ', '')  # remove spaces
         result = 0
         last_num = 0
@@ -30,6 +62,7 @@ class Solution:
 
         result += last_num
         return result
+        """
 
 
         
