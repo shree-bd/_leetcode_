8cf1b3ec971294b9a967632e8adff75df149db8d
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-04-26 01:16:54 -0400

    Time: 6098 ms (5.07%) | Memory: 18.5 MB (77.72%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.py
@@ -0,0 +1,12 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        n  = len(nums)
+
+        for i in range(n):
+            if sum(nums[:i]) == sum(nums[i+1:]):
+                return i
+        return -1
+
+
+
+#  TC: O(N) | SC.: O(1)
