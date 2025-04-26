4a1d265ed55e6a72046c6b9077b282a579b87d8a
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-04-26 01:24:46 -0400

    Time: 7 ms (57.05%) | Memory: 18.8 MB (38.73%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -1,5 +1,21 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
+        total = sum(nums)
+        left_sum = 0
+
+        for i, num in enumerate(nums):
+            if left_sum == (total - left_sum - num):
+                return i
+            left_sum += num
+        return -1
+
+#  TC: O(N) | SC.: O(1)
+
+"""
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+
+
         n  = len(nums)
 
         for i in range(n):
@@ -7,6 +23,6 @@ class Solution:
                 return i
         return -1
 
+#  TC: O(N^2) | SC.: O(1)
 
-
-#  TC: O(N) | SC.: O(1)
+"""
