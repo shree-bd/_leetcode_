47ee635bebdd5f94bff8b42a0e79a4a8eee21bf8
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-04-28 00:01:01 -0400

    Time: 85 ms (38.93%) | Memory: 23.3 MB (85.80%) - LeetSync

--- 912-random-pick-with-weight/random-pick-with-weight.py
+++ 912-random-pick-with-weight/random-pick-with-weight.py
@@ -11,9 +11,17 @@ class Solution:
     def pickIndex(self) -> int:
         total = self.prefix[-1]
         target = random.randint(1, total)
-        idx = bisect.bisect_left(self.prefix, target)
-        return idx
-        
+
+        # Manual binary search
+        low, high = 0, len(self.prefix) - 1
+        while low < high:
+            mid = (low + high) // 2
+            if self.prefix[mid] < target:
+                low = mid + 1
+            else:
+                high = mid
+
+        return low
 
 
 # Your Solution object will be instantiated and called as such:
