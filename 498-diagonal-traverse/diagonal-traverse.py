5e92e052a92acf94ea781c128d6a813ef0eb4a6c
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-04-29 13:31:06 -0400

    Time: 7 ms (86.83%) | Memory: 19.5 MB (97.17%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.py
+++ 498-diagonal-traverse/diagonal-traverse.py
@@ -3,25 +3,32 @@ class Solution:
         if not mat or not mat[0]:
             return []
 
-        m, n = len(mat), len(mat[0])
+        rows, cols = len(mat), len(mat[0])
         res = []
+        r, c, direction = 0, 0, 1
 
-        for d in range(m + n - 1):
-            intermediate = []
+        for _ in range(rows*cols):
+            res.append(mat[r][c])
 
-            # Determine the starting point
-            r = 0 if d < n else d - n + 1
-            c = d if d < n else n - 1
-
-            while r < m and c >= 0:
-                intermediate.append(mat[r][c])
-                r += 1
-                c -= 1
-
-            # Reverse order for even diagonals
-            if d % 2 == 0:
-                res.extend(intermediate[::-1])
+            if direction ==1:
+                if c == cols-1:
+                    r += 1
+                    direction = -1
+                elif r == 0:                    
+                    c += 1
+                    direction = -1
+                else:
+                    r -= 1
+                    c += 1
             else:
-                res.extend(intermediate)
-
+                if r == rows - 1:
+                    c += 1
+                    direction = 1
+                elif c== 0:
+                    r += 1
+                    direction = 1
+                else:
+                    r += 1
+                    c -= 1
+                
         return res
