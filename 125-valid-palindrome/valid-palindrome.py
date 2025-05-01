c62fe9b240c619fb73f937448bd6b871f7e1b402
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-05-01 13:23:50 -0400

    Time: 14 ms (14.66%) | Memory: 18.1 MB (69.97%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,41 +1,17 @@
-# using manual aphanumeric func: time comeplxity: O(1)
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        def is_alpha(c):
+            return c.isalnum()
+
         left, right = 0, len(s)-1
 
         while left < right:
-            while left < right and not self.alphaNum(s[left]):
+            while left < right and not is_alpha(s[left]):
                 left += 1
-            while right > left and not self.alphaNum(s[right]):
+            while left < right and not is_alpha(s[right]):
                 right -= 1
             if s[left].lower() != s[right].lower():
                 return False
-            left, right = left + 1, right - 1
+            left, right = left+1,right-1
         return True
-
-    def alphaNum(self, c):
-        return (ord('A') <= ord(c) <= ord('Z') or
-                ord('a') <= ord(c) <= ord('z') or
-                ord('0') <= ord(c) <= ord('9'))
-
-# Time Complexity: O(N)
-# Space Complexity: O(1)
-
-
-#  Using built in function
-"""
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        s = ''.join(c.lower() for c in s if c.isalnum())
-
-        left, right = 0, len(s)-1
-        while left < right:
-            if s[left] != s[right]:
-                return False
-            left += 1
-            right -= 1
-        return True
-
-# Time Complexity: O(N)
-# Space Complexity: O(N)
-"""
+        
