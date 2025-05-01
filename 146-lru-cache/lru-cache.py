6ae4f7c95db382ff55cac4a3ac14bcf38a8fe621
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-05-01 00:52:32 -0400

    Time: 158 ms (36.71%) | Memory: 78.6 MB (45.98%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,76 +1,63 @@
-# class Node:
-#     def __init__(self, key, val):
-#         self.key, self.val = key, val
-#         self.prev = self.next = None
+class ListNode:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.prev = self.next = None
 
-# class LRUCache:
-#     def __init__(self, capacity: int):
-#         self.cap = capacity
-#         self.cache = {}  # Map key to node
+class LRUCache:
 
-#         # Dummy nodes for LRU (left) and MRU (right)
-#         self.left, self.right = Node(0, 0), Node(0, 0)
-#         self.left.next, self.right.prev = self.right, self.left
-
-#     # Remove a node from the doubly linked list
-#     def remove(self, node):
-#         prev, nxt = node.prev, node.next
-#         prev.next, nxt.prev = nxt, prev
-
-#     # Insert a node at the right (most recently used position)
-#     def insert(self, node):
-#         prev, nxt = self.right.prev, self.right
-#         prev.next = nxt.prev = node
-#         node.next, node.prev = nxt, prev
-
-#     def get(self, key: int) -> int:
-#         if key in self.cache:
-#             # Move the accessed node to the most recently used position
-#             self.remove(self.cache[key])
-#             self.insert(self.cache[key])
-#             return self.cache[key].val
-#         return -1
-
-#     def put(self, key: int, value: int) -> None:
-#         if key in self.cache:
-#             # Remove the old node if the key already exists
-#             self.remove(self.cache[key])
-#         self.cache[key] = Node(key, value)
-#         self.insert(self.cache[key])
-
-#         if len(self.cache) > self.cap:
-#             # Remove the least recently used node
-#             lru = self.left.next
-#             self.remove(lru)
-#             del self.cache[lru.key]  # Fix: Remove from hashmap
-
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.node_map = {}
+        self.head = self.tail = ListNode(-1,-1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
         
+
+    def get(self, key: int) -> int:
+        if key not in self.node_map:
+            return -1
+        
+        node = self.node_map[key]
+
+        self._remove(node)
+        self._add(node)
+
+        return node.val
+        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.node_map:
+            old_node = self.node_map[key]
+
+            self._remove(old_node)
+
+        node = ListNode(key, value)
+        self.node_map[key] = node
+        self._add(node)
+
+        if len(self.node_map) > self.capacity:
+            node_to_delete = self.head.next
+            self._remove(node_to_delete)
+
+            del self.node_map[node_to_delete.key]
+
+    
+    def _remove(self,node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+    def _add(self,node):
+        prev_end = self.tail.prev
+        prev_end.next = node
+
+        node.prev = prev_end
+        node.next = self.tail
+
+        self.tail.prev = node
+        
+
+
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
-
-# >>>>>>>>>>>>>> More concise soltuin using OrderedDict() <<<<<<<<<<<<<<<<<<
-
-from collections import OrderedDict
-
-class LRUCache:
-    def __init__(self, capacity: int):
-        self.cache = OrderedDict()
-        self.cap = capacity
-
-
-    def get(self, key: int) -> int:
-        if key in self.cache:
-            self.cache.move_to_end(key)
-            return self.cache[key]
-        return -1
-
-
-    def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.cache.move_to_end(key)
-        elif len(self.cache) >= self.cap:
-            self.cache.popitem(last=False)
-
-        self.cache[key] = value
