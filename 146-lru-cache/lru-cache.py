04660a19b3fa34b420c0e25aad310f2f31c476e2
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-05-01 00:56:39 -0400

    Time: 132 ms (66.27%) | Memory: 78.7 MB (27.39%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -28,31 +28,27 @@ class LRUCache:
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             old_node = self.cache[key]
-
             self._remove(old_node)
 
-        node = ListNode(key, value)
-        self.cache[key] = node
-        self._add(node)
+        new_node = ListNode(key, value)
+        self.cache[key] = new_node
+        self._add(new_node)
 
         if len(self.cache) > self.capacity:
-            node_to_delete = self.head.next
-            self._remove(node_to_delete)
-
-            del self.cache[node_to_delete.key]
-
+            lru = self.head.next
+            self._remove(lru)
+            del self.cache[lru.key]
     
     def _remove(self,node):
-        node.prev.next = node.next
-        node.next.prev = node.prev
+        prev, nxt = node.prev, node.next
+        prev.next = nxt
+        nxt.prev = prev
 
     def _add(self,node):
         prev_end = self.tail.prev
-        prev_end.next = node
-
+        prev_end.next = node 
         node.prev = prev_end
         node.next = self.tail
-
         self.tail.prev = node
         
 
