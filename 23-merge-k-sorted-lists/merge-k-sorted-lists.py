583ce97db7bbf47c0c7a14a2a5670ee5b11a2047
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-05-01 19:53:34 -0400

    Time: 8 ms (78.64%) | Memory: 20.2 MB (69.14%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -5,7 +5,8 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists: return None
+        if not lists: 
+            return None
 
         while len(lists) > 1:
             result = []
@@ -13,7 +14,7 @@ class Solution:
                 l1 = lists[i]
                 l2 = lists[i+1] if i+1 < len(lists) else None
                 result.append(self.mergelists(l1,l2))
-            lists = result        
+            lists = result
         return lists[0]
 
     def mergelists(self, list1, list2):
