c1d063634104049ffc1b7219c73c8dca01bcc09e
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-05-01 13:43:53 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (95.08%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -1,36 +1,20 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        stack = []
 
         parts = path.split("/")
 
-        for part in parts:
-            if part == "" or part == ".":
+        stack = []
+
+        for c in parts:
+            if c == "." or c == "":
                 continue
-            elif part == "..":
+            elif c == "..":
                 if stack:
                     stack.pop()
             else:
-                stack.append(part)
+                stack.append(c)
 
         return "/" + "/".join(stack)
 
-        #  /home/a/b
 
-
-             
-# Time Complexity: O(N)
-# Space Complexity: O(N)
-
-"""
-Input: "/a/./b/../../c/"
-Steps:
-- 'a' → push to stack
-- '.' → ignore
-- 'b' → push to stack
-- '..' → pop 'b'
-- '..' → pop 'a'
-- 'c' → push to stack
-
-Output: "/c"
-"""
+        
