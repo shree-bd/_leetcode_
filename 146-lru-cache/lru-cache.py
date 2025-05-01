a05a8b95f1f9cc589c7b218d94da6950ab2d3449
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-05-01 00:54:30 -0400

    Time: 132 ms (66.27%) | Memory: 78.7 MB (27.39%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -7,17 +7,17 @@ class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.node_map = {}
+        self.cache = {}
         self.head = self.tail = ListNode(-1,-1)
         self.head.next = self.tail
         self.tail.prev = self.head
         
 
     def get(self, key: int) -> int:
-        if key not in self.node_map:
+        if key not in self.cache:
             return -1
         
-        node = self.node_map[key]
+        node = self.cache[key]
 
         self._remove(node)
         self._add(node)
@@ -26,20 +26,20 @@ class LRUCache:
         
 
     def put(self, key: int, value: int) -> None:
-        if key in self.node_map:
-            old_node = self.node_map[key]
+        if key in self.cache:
+            old_node = self.cache[key]
 
             self._remove(old_node)
 
         node = ListNode(key, value)
-        self.node_map[key] = node
+        self.cache[key] = node
         self._add(node)
 
-        if len(self.node_map) > self.capacity:
+        if len(self.cache) > self.capacity:
             node_to_delete = self.head.next
             self._remove(node_to_delete)
 
-            del self.node_map[node_to_delete.key]
+            del self.cache[node_to_delete.key]
 
     
     def _remove(self,node):
