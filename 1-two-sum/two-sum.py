af6aae23f1693c5bb6d0feca828833266c7e9a72
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-07-18 11:40:49 -0400

    Time: 1688 ms (29.16%) | Memory: 18.4 MB (87.03%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,26 +1,10 @@
-"""
-LeetCode Problem: 1 Two Sum
-"""
-
-from typing import List
-"""
-LeetCode Problem: 1 Two Sum
-"""
-
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        if not nums:
-            return None
+        n = len(nums)
 
-        hashMap = {}
-
-        for i,num in enumerate(nums):
-            diff = target - num
-            if diff in hashMap:
-                return [hashMap[diff], i]
-            hashMap[num] = i
-
-        return []
-
+        for i in range(n):
+            for j in range(i+1, n):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
         
         
