809f3326a068cc161e338bbd66cc9516be4955bb
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-07-19 16:40:19 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (71.80%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,54 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i = m+n-1
+        last = m + n - 1
 
+        # merge in reverse order
         while m > 0 and n > 0:
             if nums1[m-1] > nums2[n-1]:
-                nums1[i] = nums1[m-1]
+                nums1[last] = nums1[m-1]
                 m -= 1
             else:
-                nums1[i] = nums2[n-1]
+                nums1[last] = nums2[n-1]
                 n -= 1
+            last -= 1
 
-            i = i-1
-
+        # fill nums1 with leftover nums2 elements
         while n > 0:
-            nums1[i] = nums2[n-1]
-            n -= 1
-            i -= 1
+            nums1[last] = nums2[n-1]
+            n, last = n-1, last - 1
+        
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-        """
-        res = []
-        i = j = 0
-
-        while i<m and j < n:
-            if nums1[i] < nums2[j]:
-                res.append(nums1[i])
-                i += 1
-            else:
-                res.append(nums2[j])
-                j += 1
-        while i < m:
-            res.append(nums1[i])
-            i+= 1 
-        while j < n:
-            res.append(nums2[j])
-            j+=1
-
-        for k in range(len(res)):
-            nums1[k] = res[k]
-"""
