5f9dac5e504023b5cdc947e8f391e14852355264
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-07-26 01:58:22 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (32.18%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.py
+++ 1666-make-the-string-great/make-the-string-great.py
@@ -1,13 +1,11 @@
 class Solution:
     def makeGood(self, s: str) -> str:
         s= list(s)
-        i = 0
+        stack = []
 
-        for j in range(len(s)):
-            s[i] = s[j]
-            if i > 0 and abs(ord(s[i]) - ord(s[i-1])) == 32:
-                i -= 1
+        for ch in s:
+            if stack and abs(ord(stack[-1]) - ord(ch)) == 32:
+                stack.pop()
             else:
-                i += 1
-
-        return "".join(s[:i])
+                stack.append(ch)
+        return "".join(stack)
