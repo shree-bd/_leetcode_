dcd29f6781ae40836ed03412f540ee54679cc47d
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-07-27 19:27:16 -0400

    Time: 119 ms (88.25%) | Memory: 18 MB (80.23%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,5 +1,14 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+
+        for coin in coins:
+            for x in range(coin, amount + 1):
+                dp[x] += dp[x - coin]
+        return dp[amount]
+
+        """
         dp = [[0] * (len(coins) + 1) for i in range(amount+1)]
         dp[0] = [1] * (len(coins) + 1)
 
@@ -9,4 +18,5 @@ class Solution:
                 if a - coins[i] >= 0:
                     dp[a][i] += dp[a-coins[i]][i]
         return dp[amount][0]
+        """
         
