568fca1a5feeb63669690db45a7236ebc9dc81a5
Shridhar <57531390+shree-bd@users.noreply.github.com>
2025-07-28 02:09:59 -0400

    Time: 181 ms (22.63%) | Memory: 18.2 MB (22.58%) - LeetSync

--- 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.py
+++ 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.py
@@ -1,13 +1,14 @@
 class Solution:
     def findKDistantIndices(self, nums: List[int], key: int, k: int) -> List[int]:
         n = len(nums)
-        res = []
+        res = set()
 
-        for i in range(n):
-            for j in range(n):
-                if nums[j] == key and abs(i-j)<= k:
-                    res.append(i)
-                    break
-        return res
+        key_indices = [i for i, val in enumerate(nums) if val == key]
 
-        
+        for idx in key_indices:
+            start = max(0, idx-k)
+            end = min(n-1, idx+k)
+            for i in range(start, end+1):
+                res.add(i)
+        return sorted(res)
+
